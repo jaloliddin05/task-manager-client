943a34d32c2f7edefe5795d793f8fd2125a6705a
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-11-22 19:32:18 +0500

    file structure

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,11 +4,9 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/HomePage.vue') }],
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
